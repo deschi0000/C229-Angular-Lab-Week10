a770917c80c19f6c8b8a75208b31d8723ea73e3d
deschi0000 <79345657+deschi0000@users.noreply.github.com>
2023-07-23 12:27:18 +0800

    Complete video 2

--- client/src/app/admin/auth/auth.component.ts
+++ client/src/app/admin/auth/auth.component.ts
@@ -1,42 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { User } from '../../model/user.model';
 import { AuthService } from '../../model/auth.service';
 
-import { User } from '../../model/user.model';
-
 @Component({
-  templateUrl: './auth.component.html'
+  templateUrl: './auth.component.html',
 })
-export class AuthComponent implements OnInit
-{
+export class AuthComponent implements OnInit {
   public user: User;
   public errorMessage: string;
 
   constructor(private router: Router,
               private auth: AuthService) { }
 
-  ngOnInit(): void
+  ngOnInit(): void 
   {
+    console.log('->Debug-> at the Auth Component');
     this.user = new User();
   }
 
-  authenticate(form: NgForm): void
-  {
-    if (form.valid)
-    {
-      // perform authentication
+  authenticate(form: NgForm): void {
+    if (form.valid) {
+      // Perform Authentication
       this.auth.authenticate(this.user).subscribe(data => {
-        if (data.success)
+        if(data.success)
         {
           this.auth.storeUserData(data.token, data.user);
           this.router.navigateByUrl('admin/main');
         }
       });
     }
-    else
-    {
-      this.errorMessage = 'Form Data Invalid';
+    else {
+      this.errorMessage = 'Form Data invalid'
     }
   }
 
